0942f3c3e42c11c8196c796f89432ddef6775d92
Pasi Junttila <pasi.junttila@live.com>
2023-11-17 14:38:07 +0200

    Improve number amount test.

--- test_lotto.py
+++ test_lotto.py
@@ -3,9 +3,15 @@ from lotto import lottery_numbers
 
 class TestLotteryNumbers(unittest.TestCase):
 
-  def test_lottery_numbers_amount_valid(self):
+    def test_lottery_numbers_amount_valid(self):
         result = lottery_numbers(1, 1, 100)
         self.assertEqual(len(result), 1)
+        result = lottery_numbers(10, 1, 100)
+        self.assertEqual(len(result), 10)
+        result = lottery_numbers(100, 1, 100)
+        self.assertEqual(len(result), 100)
+        result = lottery_numbers(1000, 1, 1000)
+        self.assertEqual(len(result), 1000)
 
 if __name__ == "__main__":
     unittest.main()
