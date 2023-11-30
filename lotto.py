85130028f7d0c5c27d9d45e4c16fe69328a22af0
Pasi Junttila <pasi.junttila@live.com>
2023-11-30 11:54:58 +0200

    Add formatter for lotto numbers.
    Common entry point for different flavours of Lotto.

--- lotto.py
+++ lotto.py
@@ -6,5 +6,12 @@ def lottery_numbers(amount: int, lower: int, upper: int):
     weekly_draw.sort()
     return weekly_draw
 
+def get_lottery_numbers(amount: int, start: int, end: int):
+    output = "<span class='lotto-numbers'>"
+    for number in lottery_numbers(amount, start, end):
+        output += f'{number} '
+    output += "</span>"
+    return output
+
 if __name__ == '__main__':
     pass
