5b7ca78c85fef90ada1a95f5b6891f91e5f64329
Pasi Junttila <pasi.junttila@live.com>
2023-12-07 13:27:15 +0200

    Add endpoint for Euro Jackpot.

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from lotto import get_lottery_numbers
+from lotto import get_lottery_numbers, get_eurojackpot_lottery_numbers
 
 app = Flask(__name__)
 
@@ -10,6 +10,7 @@ doc = """
     <h4>Select Lotto Flavour</h4>
     <a href='/veikkauslotto'>Veikkaus Lotto</a>
     <a href='/vikinglottery'>Viking Lotto</a>
+    <a href='/eurojackpotlotto'>Euro Jackpot Lotto</a>
   </body>
 </html>
 """
@@ -28,6 +29,12 @@ def veikkauslotto():
 def vikinglottery():
     new_numbers = get_lottery_numbers(6, 1, 48)
     output = f"<html><head><title>Viking Lotto</title></head><body><h5>New numbers for Viking Lotto</h5>{new_numbers}</body></html>"
+    return output
+
+@app.route('/eurojackpotlotto')
+def eurojackpotlotto():
+    new_numbers = get_eurojackpot_lottery_numbers()
+    output = f"<html><head><title>Viking Lotto</title></head><body><h5>New numbers for Eurojackpot Lotto</h5>{new_number>
     return output
 
 if __name__ == '__main__':
