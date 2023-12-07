35556d7a9b78f8e7ea3bb208bda61b9bbe6d2dd5
Pasi Junttila <pasi.junttila@live.com>
2023-12-07 13:57:33 +0200

    Fix variable name.

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ def vikinglottery():
 @app.route('/eurojackpotlotto')
 def eurojackpotlotto():
     new_numbers = get_eurojackpot_lottery_numbers()
-    output = f"<html><head><title>Viking Lotto</title></head><body><h5>ew numbers for Euro Jackpot Lotto</h5>{new_number}</body></html>"
+    output = f"<html><head><title>Viking Lotto</title></head><body><h5>ew numbers for Euro Jackpot Lotto</h5>{new_numbers}</body></html>"
     return output
 
 if __name__ == '__main__':
