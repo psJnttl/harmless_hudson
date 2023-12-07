7457d2b36bd91b5d37e5e4712472894d4761aaaa
Pasi Junttila <pasi.junttila@live.com>
2023-12-07 13:25:23 +0200

    Add function for eurojackpot numbers.

--- lotto.py
+++ lotto.py
@@ -13,5 +13,15 @@ def get_lottery_numbers(amount: int, start: int, end: int):
     output += "</span>"
     return output
 
+def get_eurojackpot_lottery_numbers():
+    output = "<span class='lotto-numbers'>"
+    for number in lottery_numbers(5, 1, 50):
+        output += f'{number} '
+    output += "</span><span class='star-numbers'>"
+    for number in lottery_numbers(2, 1, 12):
+        output += f'{number} '
+    output += "</span>"
+    return output
+
 if __name__ == '__main__':
     pass
