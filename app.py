9c2cd85efef0ce732b603fcb6f3f97576b3061c1
Pasi Junttila <pasi.junttila@live.com>
2023-12-07 14:23:28 +0200

    Add generator for Lotto pages.
    Remove repetition, promote uniformity.

--- app.py
+++ app.py
@@ -15,6 +15,19 @@ doc = """
 </html>
 """
 
+def lotto_page_generator(numbers: str, title: str):
+    return f"""
+<html>
+  <head>
+    <title>{title}</title>
+  </head>
+  <body>
+    <h5>New numbers for {title}</h5>
+    {numbers}
+  </body>
+</html>
+"""
+
 @app.route('/')
 def root():
     return doc
@@ -22,20 +35,17 @@ def root():
 @app.route('/veikkauslotto')
 def veikkauslotto():
     new_numbers = get_lottery_numbers(7, 1, 40)
-    output = f"<html><head><title>Veikkaus Lotto</title></head><body><h5>New numbers for Veikkaus Lotto</h5>{new_numbers}</body></html>"
-    return output
+    return return lotto_page_generator(new_numbers, "Veikkaus Lotto")
 
 @app.route('/vikinglottery')
 def vikinglottery():
     new_numbers = get_lottery_numbers(6, 1, 48)
-    output = f"<html><head><title>Viking Lotto</title></head><body><h5>New numbers for Viking Lotto</h5>{new_numbers}</body></html>"
-    return output
+    return lotto_page_generator(new_numbers, "Viking Lotto")
 
 @app.route('/eurojackpotlotto')
 def eurojackpotlotto():
     new_numbers = get_eurojackpot_lottery_numbers()
-    output = f"<html><head><title>Viking Lotto</title></head><body><h5>ew numbers for Euro Jackpot Lotto</h5>{new_numbers}</body></html>"
-    return output
+    return lotto_page_generator(new_numbers, "Euro Jackpot Lotto")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
