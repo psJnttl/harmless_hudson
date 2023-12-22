880141eb9f1f1c68282831d30525fe60113e5806
Pasi Junttila <pasi.junttila@live.com>
2023-12-22 15:22:26 +0200

    Take Bootstrap CSS into use.

--- app/app.py
+++ app/app.py
@@ -11,60 +11,86 @@ table a:hover {
     color: white;
     background-color: #0099ff;
 }
+body {
+    padding-top: 10px
+}
 """
 
 doc = f"""
 <html>
   <head>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
     <style>{style}</style>
     <title>Lotto App</title>
   </head>
   <body>
-    <h4>Select Lotto Flavour</h4>
-    <table>
-      <thead>
+    <div class="container">
+      <h4>Select Lotto Flavour</h4>
+      <table class="table table-striped">
+        <thead>
+            <tr>
+            <th scope="col">Flavour</th>
+            <th scope="col">Rules</th>
+            <th scope="col">Permutations</th>
+            </tr>
+        </thead>
+        <tbody>
           <tr>
-          <th>Flavour</th>
-          <th>Rules</th>
-          <th>Permutations</th>
+            <td><a href="/veikkauslotto">Veikkaus</a></td>
+            <td>7 unique numbers, range 1-50</td>
+            <td>18643560</td>
           </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td><a href="/veikkauslotto">Veikkaus</a></td>
-          <td>7 unique numbers, range 1-50</td>
-          <td>18643560</td>
-        </tr>
-        <tr>
-          <td><a href="/vikinglottery">Viking</a></td>
-          <td>6 unique numbers, range 1-48</td>
-          <td>12271512</td>
-        </tr>
-        <tr>
-          <td><a href="/eurojackpotlotto">Euro Jackpot</a></td>
-          <td>5 unique numbers, range 1-50 and 2 unique numbers, range 1-12</td>
-          <td>139838160</td>
-        </tr>
-        <tr>
-          <td><a href="/jokeri">Jokeri</a></td>
-          <td>7 non-unique numbers, range 0-9</td>
-          <td>10000000</td>          
-        </tr>
-      </tbody>
-    </table>
+          <tr>
+            <td><a href="/vikinglottery">Viking</a></td>
+            <td>6 unique numbers, range 1-48</td>
+            <td>12271512</td>
+          </tr>
+          <tr>
+            <td><a href="/eurojackpotlotto">Euro Jackpot</a></td>
+            <td>5 unique numbers, range 1-50 and 2 unique numbers, range 1-12</td>
+            <td>139838160</td>
+          </tr>
+          <tr>
+            <td><a href="/jokeri">Jokeri</a></td>
+            <td>7 non-unique numbers, range 0-9</td>
+            <td>10000000</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </body>
 </html>
+"""
+
+style += """
+.star-numbers {
+    padding-left: 5px
+}
 """
 
 def lotto_page_generator(numbers: str, title: str):
     return f"""
 <html>
   <head>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+    <style>{style}</style>
     <title>{title}</title>
   </head>
   <body>
-    <h5>New numbers for {title}</h5>
-    {numbers}
+    <div class="container">
+      <table>
+        <thead>
+          <tr>
+            <th scope="col">New numbers for {title}</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>{numbers}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </body>
 </html>
 """
